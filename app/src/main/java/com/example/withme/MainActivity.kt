dc84fc0c19da14e1840cbc383d5d8484822c731b
Euna-6 <Euna-6>
2023-10-11 18:40:41 +0900

    popupAcivity 등록

--- app/src/main/java/com/example/withme/MainActivity.kt
+++ app/src/main/java/com/example/withme/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.withme
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.withme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.calendarView.setOnDateChangeListener { view, year, month, dayOfMonth ->
+            val intent = Intent(this, PopupActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
