949a2d46dcd628f14c53d70ba090cd45809079d1
Euna-6 <Euna-6>
2023-10-17 16:25:54 +0900

    SignupActivity : update (b-day 등록)

--- app/src/main/java/com/example/withme/SignupActivity.kt
+++ app/src/main/java/com/example/withme/SignupActivity.kt
@@ -1,7 +1,10 @@
 package com.example.withme
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.DatePicker
 import com.example.withme.databinding.ActivitySignupBinding
 
 class SignupActivity : AppCompatActivity() {
@@ -11,6 +14,28 @@ class SignupActivity : AppCompatActivity() {
         val binding = ActivitySignupBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        binding.switchSignupBday.setOnCheckedChangeListener{ btnview,isChecked ->
+            if (isChecked) {
+                DatePickerDialog(this, object: DatePickerDialog.OnDateSetListener {
+                    override fun onDateSet(view: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                        binding.tvSignupBday.setText("$year.${month+1}.$dayOfMonth")
+                    }
+                }, 2000, 6, 1).show()
+                binding.tvSignupBday.visibility = View.VISIBLE
+            } else {
+                binding.tvSignupBday.visibility = View.INVISIBLE
+            }
+        }
+
+
+        /*
+        binding.chbxSignup.setOnClickListener{
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+
+            }
+        }
+
+         */
 
     }
 }
