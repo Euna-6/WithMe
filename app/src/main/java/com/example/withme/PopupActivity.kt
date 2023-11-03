18b4e617550286041867b8dc2f63f7d0a0cc01b8
Euna-6 <Euna-6>
2023-11-03 11:39:03 +0900

    일정 확인 UI : 231103_1

--- app/src/main/java/com/example/withme/PopupActivity.kt
+++ app/src/main/java/com/example/withme/PopupActivity.kt
@@ -1,6 +1,7 @@
 package com.example.withme
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
 import android.view.Window
 import com.example.withme.databinding.ActivityMainPopupBinding
@@ -12,7 +13,8 @@ class PopupActivity : Activity() {
         setContentView(binding.root)
 
         binding.btnPopupRegister.setOnClickListener{
-
+            val intent = Intent(this, RegisterScheduleActivity::class.java)
+            startActivity(intent)
         }
 
         binding.btnPopupBack.setOnClickListener{
